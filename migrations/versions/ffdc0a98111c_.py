8d08a08f66e6e48fa86eab09aaeae6028127aef1
david <ypchou.david@gmail.com>
2022-11-23 13:36:59 -0800

    changed revision id

---
+++ migrations/versions/ffdc0a98111c_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: ffdc0a98111c
+Revises: 0a07a9efab5b
+Create Date: 2022-11-23 13:36:21.364087
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ffdc0a98111c'
+down_revision = '0a07a9efab5b'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
