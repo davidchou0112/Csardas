a110321bdd8b659b111553829d34e215c355cc41
david <ypchou.david@gmail.com>
2022-11-22 22:10:31 -0800

    try this one

---
+++ migrations/versions/ffdc0a98111c_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: ffdc0a98111c
+Revises: f864c2f45522
+Create Date: 2022-11-22 22:09:58.727717
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ffdc0a98111c'
+down_revision = 'f864c2f45522'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
