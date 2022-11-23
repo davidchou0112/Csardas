e8ee0f3bdc279869c1a83cc4bebaa2e28e6ac833
david <ypchou.david@gmail.com>
2022-11-23 13:27:05 -0800

    added seed image data, next: merge to main

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,6 +1,6 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
-
+from .images import seed_images, undo_images
 from app.models.db import db, environment, SCHEMA
 
 # Creates a seed group to hold our commands
@@ -16,13 +16,16 @@ def seed():
         # command, which will  truncate all tables prefixed with 
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
+        undo_images()
         undo_users()
     seed_users()
+    seed_images()
     # Add other seed functions here
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
+    undo_images()
     undo_users()
     # Add other undo functions here
