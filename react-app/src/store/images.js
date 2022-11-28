886efcac66019086f94b57c65da6b30865024d12
david <ypchou.david@gmail.com>
2022-11-27 19:47:53 -0800

    front end images

--- react-app/src/store/images.js
+++ react-app/src/store/images.js
@@ -1,7 +1,49 @@
-import { csrfFetch } from './csrf';
+// import { csrfFetch } from './csrf';
+
+
+// CONSTANTS 
 
 const GET_ALL_IMAGES = 'images/displayAllImages';
 const GET_SINGLE_IMAGE = 'images/displaySingleImage';
 const POST_IMAGE = 'images/postImage';
 const UPDATE_IMAGE = 'images/updateImage';
 const DELETE = 'images/deleteImage';
+
+// REGULAR ACTION CREATOR
+
+const displayAllImages = (images) => {
+    return {
+        type: GET_ALL_IMAGES,
+        images
+    }
+}
+
+// THUNK
+
+export const getAllImages = () => async dispatch => {
+    const response = await fetch(`/api/images`);
+
+    if (response.ok) {
+        const data = await response.json()
+        dispatch(displayAllImages(data));
+    }
+}
+
+// REDUCER
+
+const initialState = { allImages: {}, singleImage: {} };
+const imagesReducer = (state = initialState, action) => {
+    let newState = {};
+    switch (action.type) {
+        case GET_ALL_IMAGES:
+            newState = {
+                ...state,
+                allImages: { ...action.images }
+            }
+            return newState
+
+        default:
+            return { ...state };
+    }
+}
+export default imagesReducer;
