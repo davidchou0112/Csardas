9eb1a0c3179be6ab226adcfb55bea669058f1028
david <ypchou.david@gmail.com>
2022-11-28 16:27:03 -0800

    post image works

--- react-app/src/components/PostImage/postImage.js
+++ react-app/src/components/PostImage/postImage.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
 import UploadPicture from '../CreateImage';
 
 const PostImage = () => {
-    const dispatch = useDispatch();
-
-
 
     return (
         <div>
             <h1>Post Image Here</h1>
-
             <UploadPicture />
         </div>
     )
