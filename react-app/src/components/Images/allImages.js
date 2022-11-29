9fdb13bcab1ac8483a3917350f42ded5f240e690
david <ypchou.david@gmail.com>
2022-11-28 23:58:16 -0800

    issue with state in comments.length in allImages.js

--- react-app/src/components/Images/allImages.js
+++ react-app/src/components/Images/allImages.js
@@ -1,15 +1,19 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+// import { getAllComments } from '../../store/comments';
 import { getAllImages } from '../../store/images';
 
 const AllImages = () => {
     const dispatch = useDispatch();
-    const images = useSelector(state => state.images.allImages)
+    const images = useSelector(state => state.images?.allImages)
+    const comments = useSelector(state => Object.values(state.comments?.allComments))
+
     // console.log('~~~~~~~~~this is image:', images)
 
     useEffect(() => {
         dispatch(getAllImages())
+        // dispatch(getAllComments())
     }, [dispatch])
 
     // console.log('~~~~~~~~Object.values(image)', Object.values(images).map(image => (image)))
@@ -18,6 +22,8 @@ const AllImages = () => {
         <div>
             <h1>Home Page</h1>
             {Object.values(images).map(image => (
+
+
                 <NavLink to={`/images/${image.id}`}>
                     <img src={image?.image_url} alt='pic didnt load' />
                     <br></br>
@@ -26,8 +32,12 @@ const AllImages = () => {
                     {image?.description}
                     <br></br>
                     Likes: {image?.likes.length}
+                    <br></br>
+                    Comments: {comments?.length}
                 </NavLink>
+
             ))}
+
         </div>
     )
 }
