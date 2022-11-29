ecc32d07759fbe188e80590d283b96c1e9230422
david <ypchou.david@gmail.com>
2022-11-28 17:33:00 -0800

    single image displays

--- app/__init__.py
+++ app/__init__.py
@@ -18,7 +18,7 @@ from .boto3 import (
 app = Flask(__name__, static_folder='../react-app/build', static_url_path='/')
 
 # boto3 aws
-print('~~~~~~~~am i getting here 1')
+# print('~~~~~~~~am i getting here 1')
 @app.route("/api/users/aws", methods=["POST"])
 def upload_image():
     # if "image" not in request.files:
@@ -41,40 +41,10 @@ def upload_image():
         return upload, 400
 
     url = upload["url"]
-    print("-----------", url)
+    # print("-----------", url)
     return url
 
-# @login_required
-# def upload_image():
-#     print('~~~~~~~am i getting here 2')
-#     if "image" not in request.files:
-#         return {"errors": "image required"}, 400
-
-#     image = request.files["image"]
-
-#     if not allowed_file(image.filename):
-#         return {"errors": "file type not permitted"}, 400
-    
-#     image.filename = get_unique_filename(image.filename)
-
-#     upload = upload_file_to_s3(image)
-
-#     if "url" not in upload:
-#         # if the dictionary doesn't have a url key
-#         # it means that there was an error when we tried to upload
-#         # so we send back that error message
-#         return upload, 400
-
-#     url = upload["url"]
-#     # flask_login allows us to get the current user from the request
-#     new_image = Image(user=current_user, image_url=url)
-#     db.session.add(new_image)
-#     db.session.commit()
-#     return url
-
-print('~~~~~~~~~am i getting here 3')
-
-
+# print('~~~~~~~~~am i getting here 3')
 
 # Setup login manager
 login = LoginManager(app)
