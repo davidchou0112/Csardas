1a4c4fd00e7c43d43474234bd41b058ece5ae4ea
david <ypchou.david@gmail.com>
2022-11-29 01:59:06 -0800

    nav bar css, clean up

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,37 +1,54 @@
-
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
+import './NavBar.css'
 
 const NavBar = () => {
-  return (
-    <nav>
-      <ul>
-        <li>
+
+  const user = useSelector(state => state.session.user);
+
+  if (!user) {
+    return (
+      <div className='header_wrapper'>
+        <div className='header_left'>
           <NavLink to='/' exact={true} activeClassName='active'>
             Home
           </NavLink>
-        </li>
-        <li>
+        </div>
+
+        <div className='header_right'>
           <NavLink to='/login' exact={true} activeClassName='active'>
             Login
           </NavLink>
-        </li>
-        <li>
+
           <NavLink to='/sign-up' exact={true} activeClassName='active'>
             Sign Up
           </NavLink>
-        </li>
-        <li>
-          <NavLink to='/users' exact={true} activeClassName='active'>
-            Users
-          </NavLink>
-        </li>
-        <li>
-          <LogoutButton />
-        </li>
-      </ul>
-    </nav>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className='header_wrapper'>
+
+      <div className='header_left'>
+        <NavLink to='/' exact={true} activeClassName='active'>
+          Home
+        </NavLink>
+      </div>
+
+
+      <div className='header_right'>
+        <NavLink to='/users' exact={true} activeClassName='active'>
+          Users
+        </NavLink>
+
+        <LogoutButton />
+      </div>
+
+    </div>
   );
 }
 
