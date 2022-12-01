5f36e2f26fcf1d8e730f5e9e094b06e1058ab22f
david <ypchou.david@gmail.com>
2022-12-01 06:02:47 -0800

    post image validation

--- react-app/src/components/CreateImage/index.js
+++ react-app/src/components/CreateImage/index.js
@@ -15,46 +15,60 @@ const UploadPicture = () => {
     const user_id = useSelector(state => state.session.user)
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
+    const [error, setError] = useState([]);
+    let errors = [];
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        setError([]);
         const formData = new FormData();
         formData.append("image", image);
 
+        if (!title || title.length < 1 || title.length > 20) errors.push("*Must have a title that is less than 20 characters.");
+        if (!description || description.length < 1 || description.length > 500) errors.push("*Must have a description that is less than 500 characters.");
+        if (!image) errors.push('*Must upload an image file')
+        if (image) {
+            if (!image.type.endsWith('pdf' || 'png' || 'jpg' || 'jpeg' || 'gif')) errors.push('*Only image file type allowed.')
+        }
+        setError(errors);
 
         // aws uploads can be a bit slow—displaying
         // some sort of loading message is a good idea
-        setImageLoading(true);
+        if (errors.length) {
+            return
+        } else {
+            setImageLoading(true);
 
-        // const res = await fetch('/api/images', {
-        await fetch(`/api/users/aws`, {
-            method: "POST",
-            body: formData,
-        })
-            .then(async (url) => {
-                // console.log('did you get in here????')
-                let imgUrl = await url.text()
-                const newImage = {
-                    title,
-                    description,
-                    image_url: imgUrl
-                }
-                // res is used incase you want to check for if res.ok
-                const res = await csrfFetch(`/api/users/${user_id.id}/images`, {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json"
-                    },
-                    body: JSON.stringify(newImage),
+            // const res = await fetch('/api/images', {
+            await fetch(`/api/users/aws`, {
+                method: "POST",
+                body: formData,
+            })
+                .then(async (url) => {
+                    // console.log('did you get in here????')
+                    let imgUrl = await url.text()
+                    const newImage = {
+                        title,
+                        description,
+                        image_url: imgUrl
+                    }
+                    // res is used incase you want to check for if res.ok
+                    const res = await csrfFetch(`/api/users/${user_id.id}/images`, {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json"
+                        },
+                        body: JSON.stringify(newImage),
+                    })
+                    if (res.ok) {
+                        setImageLoading(false)
+                    }
                 })
-                if (res.ok) {
-                    setImageLoading(false)
-                }
-            })
-            .catch(() => {
-                alert('failed!')
-            })
-        history.push('/')
+                .catch(() => {
+                    alert('failed!')
+                })
+            history.push('/')
+        }
     }
 
     const updateImage = (e) => {
@@ -64,6 +78,11 @@ const UploadPicture = () => {
 
     return (
         <form className="post_image_input_wrapper" onSubmit={handleSubmit}>
+            <div>
+                {error.map((error, ind) => (
+                    <div key={ind}>{error}</div>
+                ))}
+            </div> <br></br>
             <label>Title
             </label>
             <input
