e9583a7ec6d0eef3edb0ca6a3d7385064e6f81a8
david <ypchou.david@gmail.com>
2023-01-06 14:52:32 -0800

    tag names display

--- react-app/src/components/Tags/alltags.js
+++ react-app/src/components/Tags/alltags.js
@@ -2,27 +2,41 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllImages } from '../../store/images';
 import { getAllTags } from '../../store/tags';
+import './AllTags.css'
 
 const AllTags = () => {
     const dispatch = useDispatch();
-    // const [isLoaded, setIsLoaded] = useState(false);
 
     const allTags = useSelector(state => state.tags?.allTags)
-    console.log(`this is tags, from alltags.js -- :`, allTags)
+    const allTagsArr = Object.values(allTags)
 
-    // const allTagsArr = Object.values(tags.AllTags)
-    // console.log(`this is allTagsArr, from alltags.js --:`, allTagsArr)
+    // looping through allTagsArr and putting names only into tagNames
+    let tagNames = [];
+    for (let i = 0; i < allTagsArr.length; i++) {
+        if (!tagNames.includes(allTagsArr[i].name)) {
+            tagNames.push(allTagsArr[i].name)
+        }
+    }
+    // console.log(`this is tagNames --: `, tagNames)
 
     useEffect(() => {
         dispatch(getAllTags())
         dispatch(getAllImages())
-        // .then(() => setIsLoaded(true))
     }, [])
 
     return (
 
         <div id='background_ashes'>
             <h1>This will be all tags</h1>
+            <div>
+                {tagNames.map((tagname) => {
+                    return (
+                        <div id='tagName'>
+                            {tagname}
+                        </div>
+                    );
+                })}
+            </div>
         </div>
 
     )
