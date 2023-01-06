f5b89858b472575b6a60c8a54d753345d2107e5f
david <ypchou.david@gmail.com>
2023-01-06 13:21:34 -0800

    tags

--- react-app/src/components/Tags/alltags.js
+++ react-app/src/components/Tags/alltags.js
@@ -1,15 +1,23 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { getAllImages } from '../../store/images';
 import { getAllTags } from '../../store/tags';
 
 const AllTags = () => {
     const dispatch = useDispatch();
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    const tags = useSelector(state => state.tags)
+    console.log(`this is tags, from alltags.js -- :`, tags)
+
+    // const allTagsArr = Object.values(tags.AllTags)
+    // console.log(`this is allTagsArr, from alltags.js --:`, allTagsArr)
 
     useEffect(() => {
         dispatch(getAllTags())
         dispatch(getAllImages())
-    })
+            .then(() => setIsLoaded(true))
+    }, [dispatch])
 
     return (
 
