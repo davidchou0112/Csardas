0e984d1f8edf55fd1ddb82a595e08e1932f114ed
david <ypchou.david@gmail.com>
2023-01-10 05:03:04 -0800

    changed comments, clean up code. issue with single tags still present

--- react-app/src/components/Tags/AllTagsBigCard.js
+++ react-app/src/components/Tags/AllTagsBigCard.js
@@ -9,32 +9,19 @@ const TagBigCard = ({ tagname, images, tags }) => {
 
     const imagesArr = Object.values(images);
     // console.log(`this is imagesArr, from AllTagsBigCard -- : `, imagesArr)
-
-    // Filters all the tags that matches the tagname in url
     const filteredTags = tagsArr.filter(tag => tag?.name === tagname);
 
-    // Loops through the filtered tags and pushes all the imageIds of those tags into an array
     let eventIdArr = [];
-
     for (let i = 0; i < filteredTags.length; i++) {
         if (!eventIdArr.includes(filteredTags[i].imageId)) {
             eventIdArr.push(filteredTags[i].imageId)
         }
     }
 
-    // Filters all the images that matches the imageIds in the eventIdArr (All the images that have the tagname in url)
     const filteredByTag = imagesArr.filter(image => eventIdArr.includes(image?.id))
-
-    // Filters all the images whose title matches the tagname in url
     const filteredByTitle = imagesArr.filter(image => image?.title.toLowerCase().includes(tagname?.toLowerCase()))
-
-    // Comining the two arrays of images (Tagged and Title) into a single array
     const finalFiltered = filteredByTag.concat(filteredByTitle)
-
-    // Creates a random number between 0 and the length of the finalFiltered array
     const randomNum = Math.floor(Math.random() * finalFiltered.length)
-
-    // Picks a random image from the finalFiltered array
     const randomImage = finalFiltered[randomNum]
 
     return (
--- react-app/src/components/Tags/SingleTag.js
+++ react-app/src/components/Tags/SingleTag.js
@@ -20,32 +20,28 @@ const SingleTag = () => {
     const filteredTags = tagsArr.filter(tag => tag.name === tagname);
     // console.log(`this is filteredTags, from SingleTag.js -- :`, filteredTags)
 
-    // Loops through the filtered tags and pushes all the imageIds of those tags into an array
     let eventIdArr = [];
-
     for (let i = 0; i < filteredTags.length; i++) {
         if (!eventIdArr.includes(filteredTags[i].imageId)) {
             eventIdArr.push(filteredTags[i].imageId)
         }
     }
 
-    // Filters all the images that matches the imageIds in the eventIdArr (All the images that have the tagname in url)
     const filteredByTag = imagesArr.filter(image => eventIdArr.includes(image.id))
-
-    // Filters all the images whose title matches the tagname in url
     const filteredByTitle = imagesArr.filter(image => image.title.toLowerCase().includes(tagname.toLowerCase()))
-
-    // Comining the two arrays of images (Tagged and Title) into a single array
     const combinedFiltered = filteredByTag.concat(filteredByTitle)
 
-    // Loops thorugh combined filtered array and only pushes the unique images into a new array
     let finalFiltered = [];
-
     for (let i = 0; i < combinedFiltered.length; i++) {
         if (!finalFiltered.includes(combinedFiltered[i])) {
             finalFiltered.push(combinedFiltered[i])
         }
     }
+
+    useEffect(() => {
+        dispatch(getAllTags())
+        dispatch(getAllImages())
+    }, [dispatch])
 
     let results;
 
@@ -81,11 +77,6 @@ const SingleTag = () => {
             </div>
         )
     }
-
-    useEffect(() => {
-        dispatch(getAllTags())
-        dispatch(getAllImages())
-    }, [dispatch])
 
     return (
         <>
